38d89876748eed81cc7991def24a7850d4547384
firsttechnomancer <firsttechnomancer@pm.me>
2025-05-26 12:31:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,7 @@ def current_time():
         }), 503
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
+
